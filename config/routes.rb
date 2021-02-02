cec7942473888a9b3f739d06c664b2822fdb59ec
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-02 16:59:29 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  resources :users, only: :show
   resources :posts do
     resources :comments, only: :create do
       resources :likes, only: [:create, :destroy]
