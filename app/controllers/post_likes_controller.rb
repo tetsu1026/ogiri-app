5253850677ada85efa9c6112f23f00b6c8caf11d
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-05 21:20:58 +0900

    レイアウト変更

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -1,9 +1,11 @@
 class PostLikesController < ApplicationController
   
   def create
+    
     @post = Post.find(params[:post_id])
     @post_like = PostLike.create(user_id: current_user.id, post_id: @post.id)
     redirect_to root_path(@post)
+    
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = @user.posts
-     
-     post_likes = PostLike.where(user_id: current_user.id).pluck(:post_id)
-     @post_like = Post.find(post_likes)
-
+    
+     #post_likes = PostLike.where(user_id: current_user.id).pluck(:post_id)
+     #@post_like = Post.find(post_likes)
+     #@post_like = PostLike.where(user_id: current_user.id)
+     @post_like = @user.post_likes
+    
   end
 end
