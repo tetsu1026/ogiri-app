63cc1e20180e108b809eb2a00bb7f5ba05b1bead
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-05 17:48:11 +0900

    マイページにお気に入りの投稿ページの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = @user.posts
+     
+     post_likes = PostLike.where(user_id: current_user.id).pluck(:post_id)
+     @post_like = Post.find(post_likes)
+
   end
 end
