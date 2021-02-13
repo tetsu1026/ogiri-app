2e213d1529a914fdf64d93ba86f2509ac5944866
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-13 15:16:23 +0900

    レイアウト編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile, :image])
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
-class UsersController < ApplicationController
-  
+class UsersController < ApplicationController  
   def show
     @user = User.find(params[:id])
     @post = @user.posts
-    
-     #post_likes = PostLike.where(user_id: current_user.id).pluck(:post_id)
-     #@post_like = Post.find(post_likes)
-     #@post_like = PostLike.where(user_id: current_user.id)
-     @post_like = @user.post_likes
-    
+    @post_like = @user.post_likes
   end
+
 end
